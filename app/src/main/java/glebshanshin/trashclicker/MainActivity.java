b76ac2959ed04a844fd4239b4499d171371a4684
Gleb <gleb270@mail.ru>
2018-04-21 23:22:21 +0700

    21-04-2018

---
+++ app/src/main/java/glebshanshin/trashclicker/MainActivity.java
@@ -0,0 +1,30 @@
+package glebshanshin.trashclicker;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toStart(View view) {
+        Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void toExit(View view) {
+        finish();
+    }
+}
---
+++ app/src/main/java/glebshanshin/trashclicker/PlayActivity.java
@@ -0,0 +1,24 @@
+package glebshanshin.trashclicker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class PlayActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.play_main);
+    }
+    public void toMenu(View view) {
+        Intent intent1 = new Intent(PlayActivity.this, MainActivity.class);
+        startActivity(intent1);
+    }
+}
