7c8c29fa1bb1be7795cacd15ea4f16be0351f577
Gleb <gleb270@mail.ru>
2018-04-26 22:12:03 +0700

    26-04-2018

---
+++ app/src/main/java/glebshanshin/trashclicker/StoreActivity.java
@@ -0,0 +1,19 @@
+package glebshanshin.trashclicker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StoreActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.store_main);
+    }
+}
