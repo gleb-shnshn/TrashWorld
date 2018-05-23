b271e2c6d5d7ae4fa6980347afebf609d45d96b5
Gleb <gleb270@mail.ru>
2018-05-23 21:44:29 +0700

    23-05-2018 some bags

--- app/src/main/java/glebshanshin/trashworld/StorageActivity.java
+++ app/src/main/java/glebshanshin/trashworld/StorageActivity.java
@@ -93,13 +93,13 @@ public class StorageActivity extends Activity {
             lin1.setBackground(getDrawable(R.drawable.emptyqr));
         } else {
             lin1.setBackground(getDrawable(R.drawable.lookqr));
-            text1.setText(code1.substring(2, 6) + " TSH");
+            text1.setText(Integer.parseInt(code1.substring(2,5))+code1.substring(5, 6) + "\nTSH");
         }
         if (code2.equals("0")) {
             lin2.setBackground(getDrawable(R.drawable.emptyqr));
         } else {
             lin2.setBackground(getDrawable(R.drawable.lookqr));
-            text2.setText(code2.substring(2, 6) + " TSH");
+            text2.setText(Integer.parseInt(code2.substring(2,5))+code2.substring(5, 6) + "\nTSH");
         }
     }
 
